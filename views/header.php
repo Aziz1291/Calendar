f10df08bd744302f792bf989eceea84293b93e68
Aziz kouki <koukiaziz@nabeul.r-iset.tn>
2026-06-11 16:30:38 +0100

    Feat: add calendar emoji favicon to all pages

--- views/header.php
+++ views/header.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="css/calendar.css?t=<?= time() ?>">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>📅</text></svg>">
 </head>
 
 <body>
